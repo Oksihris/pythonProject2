32807d0b6aaa76fc7d7bcc88594d8ca56cdd3073
Oksihris <112577436+Oksihris@users.noreply.github.com>
2023-03-19 15:39:19 +0200

    something new

---
+++ Flowers.py
@@ -0,0 +1,7 @@
+from tensorflow import keras
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+CSV_COLUMN_NAMES = ['Sparaw','Sparall']
