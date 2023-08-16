cf73442d0eca39bb7073a7c695014cd0c9a6006e
OksanaKhry <oksihris@gmail.com>
2023-08-16 16:23:48 +0300

    List

---
+++ list.py
@@ -0,0 +1,58 @@
+# mylist =["orange", "cat", "something"]
+# print(mylist)
+
+# mylist2=[]
+# print(mylist2)
+
+# for i in mylist:
+#     print(i)
+
+# if "cat" in mylist:
+#     print("yes")
+# else:
+#     print("no")
+
+# print(len(mylist))
+
+# mylist.append("table")
+# print(mylist)
+
+# mylist.insert(1, "grape")
+# print(mylist)
+
+# item = mylist.pop()
+# print(item)
+# print(mylist)
+
+# mylist.remove("orange")
+# print(mylist)
+
+# mylist.clear()
+# print(mylist)
+
+# item2 =mylist.reverse()
+# print(mylist)
+
+# list2 = sorted(mylist)
+# print(list2)
+
+# myylist =[0]*5
+# print(myylist)
+
+# myylist2 = [1,2,3,4,5]
+# new_list = myylist+myylist2
+# print(new_list)
+
+# liist =[1,2,3,4,5,6,7,8,9]
+# a = liist[::-1]
+# print(a)
+
+# list_org =["orange", "cat", "something"]
+# list_copy = list_org.copy()
+# list_copy.append("lemon")
+# print(list_copy)
+# print(list_org)
+
+a=[1,2,3,4,5,6]
+b = [i*i for i in a]
+print(b)
