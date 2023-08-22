7bda386b4e3ff99462b896989e5f2b4ab6001b06
OksanaKhry <oksihris@gmail.com>
2023-08-22 15:59:41 +0300

    start with string

---
+++ strings.py
@@ -0,0 +1,36 @@
+# my_string ='G\'HRTcvb tutry'
+# print(my_string)
+
+# char= my_string[-3]
+# print(char)
+
+# # my_string[0]='h'
+
+# substring= my_string[1:6]
+# print(substring)
+
+# name = "Ksu "
+# sentence = name + " " + char
+# print(sentence)
+
+# for i in sentence:
+#     print(i)
+
+
+# if 't' in sentence:
+#     print("yes")
+# else:
+#     print("no")
+
+myStr2 = '   Hello World   '
+myStr2 = myStr2.strip()
+print(myStr2)
+
+print(myStr2.lower())
+print(myStr2.startswith('H'))
+
+print(myStr2.find('o'))
+print(myStr2.count('l'))
+
+print(myStr2.replace('l', 'L'))
+
